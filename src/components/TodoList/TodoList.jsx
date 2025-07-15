0916e548830cdab794258a9a1ec60ff806d2de46
mattc108 <matthewcamacho108@gmail.com>
2025-07-15 08:26:33 -0500

    solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,15 @@
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
 // Add the required props
-export const TodoList = () => <>TodoList markup</>;
+export const TodoList = ({ todos }) =>
+  todos.map(todo => (
+    <TodoInfo
+      todo={{
+        id: todo.id,
+        userId: todo.userId,
+        completed: todo.completed,
+        title: todo.title,
+        user: { ...todo.user },
+      }}
+    />
+  ));
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,12 @@
 // Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+function getUserInfo(user) {
+  const userEmail = `mailto:${user.email}`;
+
+  return (
+    <a className="UserInfo" href={userEmail}>
+      {user.name}
+    </a>
+  );
+}
+
+export const UserInfo = ({ user }) => getUserInfo(user);
